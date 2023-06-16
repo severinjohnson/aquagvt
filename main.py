0a8f63bc1e623cbb881520cf795e08eb7cb8eba3
severinjohnson <131306631+severinjohnson@users.noreply.github.com>
2023-06-16 17:51:03 -0600

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hi, this is Meyorm"
-
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {"key": "value"}  # Corrected this line
-    return jsonify(data)
+def hello():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    import os
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
-
+    app.run()
 
