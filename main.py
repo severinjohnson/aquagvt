1edb3ee3a5148393c87ab2fb838098af4117c59e
severinjohnson <131306631+severinjohnson@users.noreply.github.com>
2023-06-16 15:59:42 -0600

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
-
 from flask import Flask, jsonify
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return "Hi, this is Meyorm"
-@app.route('/api/data',methods=['GET'])
+
+@app.route('/api/data', methods=['GET'])
 def get_data():
-    data= {"key","value"}
+    data = {"key": "value"}  # Corrected this line
     return jsonify(data)
-if __name__== '__main':
+
+if __name__ == '__main__':
     app.run(debug=True)
